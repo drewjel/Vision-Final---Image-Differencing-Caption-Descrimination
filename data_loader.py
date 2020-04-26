dffb84b83770736dff7f8b582929b63863080d48
Sanxing <sc3hn@virginia.edu>
2020-04-25 22:30:18 -0400

    Rearrange folder and clean up data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -6,6 +6,7 @@ Created on Tue Apr  7 09:39:55 2020
 @author: zhe
 """
 from __future__ import print_function, division
+import warnings
 import os
 import torch
 from torch.utils.data import Dataset, DataLoader
@@ -20,151 +21,99 @@ seed_num = 17
 random.seed(seed_num)
 
 # Ignore warnings
-import warnings
 warnings.filterwarnings("ignore")
 
 
-
-  
-
-def load_img(img_root, idx):
-    im = Image.open(img_root + '/' + idx + '.png') 
+def load_img(img_dir, idx):
+    im = Image.open(img_dir + '/' + idx + '.png')
     im.convert('RGB')
     im = asarray(im)
-    im1 = Image.open(img_root + '/' + idx + '_2.png') 
+    im1 = Image.open(img_dir + '/' + idx + '_2.png')
     im1.convert('RGB')
     im1 = asarray(im1)
     return im, im1
-    
 
 
 class spot_and_diff_Dataset(Dataset):
 
-    def __init__(self, csv_file, img_root, save_root, max_samples=-1,transform=None,testing=False):
-       
-        self.testing = testing
+    def __init__(self, csv_file, img_dir, save_dir, max_samples=-1, transform=None, mode='test'):
+        MODES = ['train', 'val', 'test']
+        if mode not in MODES:
+            raise ValueError('mode must be specified as one of ', MODES)
+        self.mode = mode
         self.transform = transform
-        self.img_root = img_root
-        self.data_root = csv_file
-        self.spot_and_diff_dir = save_root  
+        self.img_dir = img_dir
+        self.data_dir = csv_file
+        self.spot_and_diff_dir = save_dir
         self.prepare_dataset()
-        if testing in ['train', 'val', 'test']:
-            self.data_label_tuples = torch.load(os.path.join(self.spot_and_diff_dir, testing) + '.pt')
-        
-        
+        self.data_label_tuples = torch.load(
+            os.path.join(self.spot_and_diff_dir, mode) + '.pt')
 
     def __getitem__(self, index):
         """
         Args:
         index (int): Index
-        
+
         Returns:
         tuple: (image, target) where target is index of the target class.
         """
-        img_0, img_1, text = self.data_label_tuples[index]['img_0'], self.data_label_tuples[index]['img_1'], self.data_label_tuples[index]['sentences']
+        img_0, img_1, text = self.data_label_tuples[index]['img_0'], self.data_label_tuples[
+            index]['img_1'], self.data_label_tuples[index]['sentences']
 
         if self.transform:
             img_0 = self.transform(img_0)
             img_1 = self.transform(img_1)
             text = text
-            
 
         return img_0, img_1, text
 
     def __len__(self):
         return len(self.data_label_tuples)
-    
+
     def prepare_dataset(self):
-        
-        if os.path.exists(os.path.join(self.spot_and_diff_dir, 'train.pt'))\
-            and os.path.exists(os.path.join(self.spot_and_diff_dir, 'val.pt'))\
-            and os.path.exists(os.path.join(self.spot_and_diff_dir, 'test.pt')):
-            print('dataset already exists')
-            return 
-        
-        
-        
-        if self.testing == 'train':
-            if os.path.exists(os.path.join(self.spot_and_diff_dir, 'train.pt')):
-                print('training data exists')
-            else:
-                train = []
-                with open(self.data_root) as f:
-                    data = json.load(f)
-                    for i in range(len(data)):
-                        idx = data[i]['img_id']
-                        sentences = data[i]['sentences']
-                        img_0, img_1 = load_img(self.img_root, idx)
-                        sample = {'img_0':img_0, 'img_1': img_1, 'sentences': sentences}
-                        train.append(sample)
-                        print(i)
-        elif self.testing == 'val':
-             if os.path.exists(os.path.join(self.spot_and_diff_dir, 'val.pt')):
-                 print('valid data exists')
-             else:
-                valid = []
-                with open(self.data_root) as f:
-                    data = json.load(f)
-                    for i in range(len(data)):
-                        idx = data[i]['img_id']
-                        sentences = data[i]['sentences']
-                        img_0, img_1 = load_img(self.img_root, idx)
-                        sample = {'img_0':img_0, 'img_1': img_1, 'sentences': sentences}
-                        valid.append(sample)
-                        print(i)
-                        
-        elif self.testing == 'test':
-             if os.path.exists(os.path.join(self.spot_and_diff_dir, 'test.pt')):
-                 print('test data exists')
-             else:
-                test = []
-                with open(self.data_root) as f:
-                    data = json.load(f)
-                    for i in range(len(data)):
-                        idx = data[i]['img_id']
-                        sentences = data[i]['sentences']
-                        img_0, img_1 = load_img(self.img_root, idx)
-                        sample = {'img_0':img_0, 'img_1': img_1, 'sentences': sentences}
-                        test.append(sample)
-                        print(i)
-        
-        
-        dataset_utils.makedir_exist_ok(self.spot_and_diff_dir)
-        if self.testing == 'train':
-            torch.save(train, os.path.join(self.spot_and_diff_dir, 'train.pt'))
-        elif self.testing == 'val':
-            torch.save(valid, os.path.join(self.spot_and_diff_dir, 'val.pt'))
-        elif self.testing == 'test':
-            torch.save(test, os.path.join(self.spot_and_diff_dir, 'test.pt'))
-                    
-            
-                
-            
-save_root = './datanew'    
-data_root = './data/annotations'
-img_root = './resized_images'
-train_set = spot_and_diff_Dataset(csv_file=os.path.join(data_root,'train.json'),
-                                    img_root=img_root,
-                                    save_root = save_root,
-                                    transform=transforms.ToTensor(),
-                                    testing='train',
-                                  )  
+        saved_path = os.path.join(self.spot_and_diff_dir, self.mode + '.pt')
+        if os.path.exists(saved_path):
+            print(self.mode, 'data exists, read from', saved_path)
+        else:
+            dataset = []
+            with open(self.data_dir) as f:
+                data = json.load(f)
+                for i in range(len(data)):
+                    idx = data[i]['img_id']
+                    sentences = data[i]['sentences']
+                    img_0, img_1 = load_img(self.img_dir, idx)
+                    sample = {'img_0': img_0, 'img_1': img_1,
+                                'sentences': sentences}
+                    dataset.append(sample)
+                    print(i)
 
-valid_set = spot_and_diff_Dataset(csv_file=os.path.join(data_root,'val.json'),
-                                    img_root=img_root,
-                                    save_root= save_root,
-                                    transform=transforms.ToTensor(),
-                                    testing='val',
-                                  ) 
+            os.makedirs(self.spot_and_diff_dir, exist_ok=True)
+            torch.save(dataset, saved_path)
+            print('Saved to', saved_path)
 
-                                   
-test_set = spot_and_diff_Dataset(csv_file=os.path.join(data_root,'test.json'),
-                                    img_root=img_root,
-                                    save_root = save_root,
+if __name__ == "__main__":
+    save_dir = 'processed_data'
+    data_dir = 'data/annotations'
+    img_dir = 'data/resized_images'
+    train_set = spot_and_diff_Dataset(csv_file=os.path.join(data_dir, 'train.json'),
+                                    img_dir=img_dir,
+                                    save_dir=save_dir,
                                     transform=transforms.ToTensor(),
-                                    testing='test',
-                                  ) 
-a = valid_set[0][0]
-b = valid_set[0][1]
-c = valid_set[0][2]
-train_data = DataLoader(valid_set, batch_size = 64, shuffle=True)    
+                                    mode='train',
+                                    )
+    train_data = DataLoader(train_set, batch_size=64, shuffle=True)
+
+    # valid_set = spot_and_diff_Dataset(csv_file=os.path.join(data_dir, 'val.json'),
+    #                                 img_dir=img_dir,
+    #                                 save_dir=save_dir,
+    #                                 transform=transforms.ToTensor(),
+    #                                 mode='val',
+    #                                 )
+
+
+    # test_set = spot_and_diff_Dataset(csv_file=os.path.join(data_dir, 'test.json'),
+    #                                 img_dir=img_dir,
+    #                                 save_dir=save_dir,
+    #                                 transform=transforms.ToTensor(),
+    #                                 mode='test',
+    #                                 )
